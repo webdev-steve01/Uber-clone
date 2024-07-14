e1d617caa30b5a75939cfeb8cef44be92ece718e
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-07-14 01:57:52 +0100

    Added a feature that allows user location to display, improved the icons and changed the location SVG

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+let exactPlace = document.getElementById("position");
+
+const position = (pos) => {
+  let { longitude, latitude } = pos.coords;
+  fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+  )
+    .then((res) => res.json())
+    .then((data) => exactPlace.append(data.address.county + ", " + data.address.state))
+    .catch(() => exactPlace.append("unknown"));
+};
+const error = (error) => {
+  exactPlace.append("blocked")
+};
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position, error);
+} else {
+  console.log("geolocation is not supported");
+}
+
+
+
 const moonPath =
   "M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z";
 
