6add33ce074c5e70f73453e6871a18ba7660d9a8
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-07-16 18:40:01 +0100

    added the search filter option and improved on the style, third commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# THIS IS A CLONE OF THE UBER WEBSITE
+
+My intention was to make this website look like uber as much as i can, but i was met with a few setbacks
+
+the biggest setback i had was the lack of access to the API used by uber to get their locations, i used another API and it didn't provide me with the exact number of places, plus the way the API works restricts me from providing more locations in the search filter, the locations will display according to the state in nigeria you're in, i might update it later to be in any part of the world though.
+
+the second drawback i faced were some problems in design, some particular designs were written with libraries that as of the time of writing this files, i am unaware of, but i will definitely brush up on my knowledge and update the design, i tried to keep it similar to uber as much as i could but i will implement my own designs where i can
+
+# p.s
+
+the light mode option might seem unnecessary but i added it anyways
--- app.js
+++ app.js
@@ -6,19 +6,52 @@ const position = (pos) => {
     `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
   )
     .then((res) => res.json())
-    .then((data) => exactPlace.append(data.address.county + ", " + data.address.state))
+    .then((data) => {
+      exactPlace.textContent = data.address.county + ", " + data.address.state;
+      let holder = data.address.state;
+      let newHolder = holder.toUpperCase();
+      let updatedHolder = newHolder[0] + newHolder[1];
+      var headers = new Headers();
+      let places = [];
+      headers.append(
+        "X-CSCAPI-KEY",
+        "ZGI4ZDdpZ0YyV2N1dTcwaFgwTUREcHpkNjNXakdZa1lldW9sc1BKOA=="
+      );
+
+      var requestOptions = {
+        method: "GET",
+        headers: headers,
+        redirect: "follow",
+      };
+      fetch(
+        `https://api.countrystatecity.in/v1/countries/NG/states/${updatedHolder}/cities`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          let states = document.getElementById("state");
+          places.push(result.map((s) => s.name));
+          console.log(places);
+          let container = places[0];
+          console.log(container);
+          container.forEach((element) => {
+            const opt = document.createElement("option");
+            opt.value = element;
+            states.appendChild(opt);
+          });
+        })
+        .catch((error) => console.log("error", error));
+    })
     .catch(() => exactPlace.append("unknown"));
 };
 const error = (error) => {
-  exactPlace.append("blocked")
+  exactPlace.append("blocked");
 };
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(position, error);
 } else {
   console.log("geolocation is not supported");
 }
-
-
 
 const moonPath =
   "M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z";
