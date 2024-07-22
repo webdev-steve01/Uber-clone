aca19e70531a9a445c732309248d5496719799dc
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-07-22 21:52:32 +0100

    added a login page and authentication

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const { platform } = require('process')
+const { Template } = require('webpack')
+
+module.exports = {
+  mode: "development",
+  entry: "./components/src/auth.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: true
+};
