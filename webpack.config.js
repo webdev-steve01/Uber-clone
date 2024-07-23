279ccc577551aa901b38c785782362617ba302bf
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-07-23 10:47:15 +0100

    improved

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
+
 const path = require("path");
 const { platform } = require("process");
 const { Template } = require("webpack");
 
 module.exports = {
   mode: 'production',
-=======
-const path = require('path')
-const { platform } = require('process')
-const { Template } = require('webpack')
-
-module.exports = {
-  mode: "development",
->>>>>>> 3f89da8275cdf2f0c3259ac21cc4d36f42cc0e19
   entry: "./components/src/auth.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-<<<<<<< HEAD
   watch: true
 };
-=======
-  watch: true
-};
->>>>>>> 3f89da8275cdf2f0c3259ac21cc4d36f42cc0e19
