19077dbec8d7769246ad9c41dfbecea183610bf6
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-07-23 10:33:20 +0100

    improved

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { platform } = require("process");
+const { Template } = require("webpack");
+
+module.exports = {
+  mode: 'production',
+  entry: "./components/src/auth.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: true,
+};
